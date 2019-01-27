1b5d53a0083d0237344d36a2c30c0597c4c70b16
junsmust <isp3087@gmail.com>
2019-01-27 20:23:53 +0900

    test

--- app/src/main/java/yaksok/dodream/com/yaksok/AddYourFmaily.java
+++ app/src/main/java/yaksok/dodream/com/yaksok/AddYourFmaily.java
@@ -59,6 +59,7 @@ public class AddYourFmaily extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_TITLE);
         actionBar.setIcon(R.drawable.chat2);
